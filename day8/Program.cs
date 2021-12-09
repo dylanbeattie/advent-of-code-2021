47c98cfced1d9bded4f3605e185d2facfb7fe4eb
dylanbeattie <dylan@dylanbeattie.net>
2021-12-09 01:25:58 +0000

    Super terse syntax for day 8

--- day8/Program.cs
+++ day8/Program.cs
@@ -1,61 +1,33 @@
-﻿var inputs = File.ReadAllLines("input.txt")
-    .Select(line => new TestCase(line));
+﻿var solutions = File.ReadAllLines("input.txt").Select(Solve).ToList();
 
-var solutions = inputs.Select(input => input.Solve());
-
-var allDigits = String.Join("", solutions.ToArray());
-var totalPart1 = allDigits.GroupBy(c => c)
-    .Where(group => "1478".Contains(group.Key))
-    .Sum(group => group.Count());
+var totalPart1 = solutions.Aggregate(String.Empty, (s1,s2) => s1 + s2)
+    .GroupBy(c => c).Where(group => "1478".Contains(group.Key)).Sum(group => group.Count());
 
 var totalPart2 = solutions.Sum(Int32.Parse);
 
 Console.WriteLine($"solution to part 1: {totalPart1}");
 Console.WriteLine($"solution to part 2: {totalPart2}");
 
+string Solve(string line) => new Observation(line).Solve();
 
-class TestCase {
-    private Dictionary<char, int> Map = new();
-    public TestCase(string line) {
+class Observation {
+    private static readonly List<int> patterns = new() { 119, 36, 93, 109, 46, 107, 123, 37, 127, 111 };
+    private readonly Dictionary<char, int> map = new();
+    private readonly string[] digits;
+    public Observation(string line) {
         var tokens = line.Split('|');
-        InputPatterns = tokens[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        DigitPatterns = tokens[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        BuildMap();
+        var inputs = tokens[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        digits = tokens[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var groups = tokens[0].GroupBy(c => c).ToList();
+        map.Add(groups.First(g => g.Count() == 4).Key, 4);
+        map.Add(groups.First(g => g.Count() == 6).Key, 1);
+        map.Add(groups.First(g => g.Count() == 9).Key, 5);
+        map.Add(inputs.First(c => c.Length == 2).ToArray().First(c => !map.ContainsKey(c)), 2);
+        map.Add(inputs.First(c => c.Length == 3).ToArray().First(c => !map.ContainsKey(c)), 0);
+        map.Add(inputs.First(c => c.Length == 4).ToArray().First(c => !map.ContainsKey(c)), 3);
+        map.Add("abcdefg".ToArray().First(c => !map.ContainsKey(c)), 6);
     }
 
-    private static Dictionary<int, int> patterns = (new[] {
-            "1110111", "0010010", "1011101", "1011011", "0111010",
-            "1101011", "1101111", "1010010", "1111111", "1111011"
-        }).Select((item, index) => (item: new String(item.Reverse().ToArray()), index))
-        .ToDictionary(pair => Convert.ToInt32(pair.item, 2), pair => pair.index);
-
-    private void BuildMap() {
-        var groups = String.Join("", InputPatterns).GroupBy(c => c);
-        Console.WriteLine("==========");
-        foreach (var group in groups) {
-            Console.WriteLine($"{group.Key}: {group.Count()}");
-        }
-        Map.Add(groups.First(g => g.Count() == 4).Key, 4);
-        Map.Add(groups.First(g => g.Count() == 6).Key, 1);
-        Map.Add(groups.First(g => g.Count() == 9).Key, 5);
-        var signal1 = InputPatterns.First(c => c.Length == 2).ToArray().First(c => !Map.ContainsKey(c));
-        Map.Add(signal1, 2);
-        var signal2 = InputPatterns.First(c => c.Length == 3).ToArray().First(c => !Map.ContainsKey(c));
-        Map.Add(signal2, 0);
-        var signal3 = InputPatterns.First(c => c.Length == 4).ToArray().First(c => !Map.ContainsKey(c));
-        Map.Add(signal3, 3);
-        Map.Add("abcdefg".ToArray().First(c => !Map.ContainsKey(c)), 6);
-    }
-
-    private int Decode(string digit) {
-        var bitField = digit.Select(c => 1 << Map[c]).Aggregate(0, (x, y) => x | y);
-        return patterns[bitField];
-    }
-
-    public string Solve() {
-        return String.Join("", DigitPatterns.Select(Decode).Select(i => i.ToString()).ToArray());
-    }
-
-    public string[] InputPatterns { get; set; }
-    public string[] DigitPatterns { get; set; }
+    private int Decode(string digit) => patterns.IndexOf(digit.Select(c => 1 << map[c]).Aggregate(0, (x, y) => x | y));
+    public string Solve() => digits.Select(Decode).Aggregate(String.Empty, (s1, s2) => s1 + s2);
 }
