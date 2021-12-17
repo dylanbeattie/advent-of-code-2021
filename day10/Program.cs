87b2dc33f2a42c85884f25d7809b1cd9b99b93f4
dylanbeattie <dylan@dylanbeattie.net>
2021-12-17 16:00:27 +0000

    Day 17 work in progress

--- day10/Program.cs
+++ day10/Program.cs
@@ -1,55 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
 var inputs = File.ReadAllLines("input.txt");
 
-var part1Solution = inputs
-             .Select(FindFirstCorruptCharacter)
-             .Where(c => c > 1)
-             .Select(scorePart1)
-             .Sum();
+var part1Solution = inputs.Select(FindFirstCorruptCharacter).Where(c => c > 1).Sum(ScorePart1);
 
-Console.WriteLine(part1Solution);
-
-//foreach (var input in inputs) {
-//    if (FindFirstCorruptCharacter(input) != 1) continue;
-//    Console.WriteLine($"{input}    {new String(FindCompletionSequence(input))}");
-//}
-var foo = "])}>".Aggregate<char, long>(0, (s, c) => (s * 5) + scorePart2(c));
-Console.WriteLine($"TEST: {foo}");
+Console.WriteLine($"Solution to part 1: {part1Solution}");
 
 var part2Scores = inputs
     .Where(line => FindFirstCorruptCharacter(line) == 1)
     .Select(FindCompletionSequence)
-    .Select(sequence => sequence.Aggregate<char,long>(0, (s, c) => (s * 5) + scorePart2(c)));
+    .Select(sequence => sequence.Aggregate<char,long>(0, (s, c) => (s * 5) + ScorePart2(c)))
+    .OrderBy(s => s).ToArray();
 
-foreach (var s in part2Scores) Console.WriteLine(s);
-
-var part2ScoresInOrder = part2Scores.OrderBy(s => s).ToArray();
-foreach(var s in part2ScoresInOrder) {}
-var howMany = part2ScoresInOrder.Length;
-var middleIndex = (howMany / 2);
-var middleScore = part2ScoresInOrder[middleIndex];
+var middleScore = part2Scores[part2Scores.Length / 2];
 
 Console.WriteLine($"Solution to part 2: {middleScore}");
 
-const string pattern = "{}<>()[]";
+int ScorePart2(char c) => " )]}>".IndexOf(c);
 
-int scorePart2(char c) {
+int ScorePart1(char c) {
     return c switch {
-        ')' => 1,
-        ']' => 2,
-        '}' => 3,
-        '>' => 4,
-        _ => 0
-    };
-}
-
-int scorePart1(char c) {
-    return c switch {
-        '(' => 1,
-        '[' => 2,
-        '{' => 3,
-        '<' => 4,
         ')' => 3,
         ']' => 57,
         '}' => 1197,
@@ -58,14 +27,16 @@ int scorePart1(char c) {
     };
 }
 
+const string pattern = "{}<>()[]";
+
 char FindFirstCorruptCharacter(string line) {
     var stack = new Stack<char>();
     foreach (var c in line) {
         var index = pattern.IndexOf(c);
-        if ((index & 1) == 1) { // closing bracket
+        if ((index & 1) == 1) { 
             var match = pattern.IndexOf(stack.Pop()) + 1;
             if (match != index) return c;
-        } else { // opening bracket
+        } else { 
             stack.Push(c);
         }
     }
@@ -78,8 +49,7 @@ char[] FindCompletionSequence(string line) {
     foreach (var c in line) {
         var index = pattern.IndexOf(c);
         if ((index & 1) == 1) { // closing bracket
-            var match = pattern.IndexOf(stack.Pop()) + 1;
-            if (match != index) throw new ArgumentException("This method only works on incomplete lines");
+            stack.Pop();
         } else { // opening bracket
             stack.Push(c);
         }
---
+++ day17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+// See https://aka.ms/new-console-template for more information
+var match = Regex.Match(File.ReadAllText("example.txt"), @"target area: x=([-0-9]+)\.\.([-0-9]+), y=([-\d]+)\.\.([-\d]+)");
+var extents = match.Groups.Values.Skip(1).Select(m => Int32.Parse(m.Value));
+
+var dyMax = 0;
+for (var dx = 0; dx < 100; dx++) {
+    for (var dy = 0; dy < 100; dy++) {
+        var x = 0, y = 0;
+        for (var step = 0; step < 100; step++) {
+            if (hit(x, y, extents))
+                x += dx;
+            y += dy;
+
+
+        }
+    }
+}
+
+
+bool hit(int x, int y, int[] extents) {
+    return (extents[0] <= x && x <= extents[1] && extents[2] <= y && y >= extents[3]);
+}
