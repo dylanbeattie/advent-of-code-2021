4977a1e3d8442e6e0b1267b2ca28c6366c93197f
Dylan Beattie <dylan@dylanbeattie.net>
2021-12-17 19:52:36 +0000

    Brute force solution to day 17

---
+++ day11/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+var octopods = File.ReadAllLines("example.txt")
+    .Select(line => line.ToCharArray().Select(c => Int32.Parse(c.ToString())).ToArray()).ToArray();
+
+int flashes = 0;
+for (var i = 0; i < 100; i++) Tick(octopods);
+
+Console.WriteLine(flashes);
+
+
+void Tick(int[][] grid)
+{
+    for (var row = 0; row < grid.Length; row++)
+    {
+        for (var col = 0; col < grid.Length; col++)
+        {
+            Flash(grid, row, col, false);
+        }
+    }
+    foreach (var row in grid)
+    {
+        Console.WriteLine(String.Join("", row.Select(e => e.ToString()).ToArray()));
+    }
+    Console.WriteLine("==========");
+}
+
+void Flash(int[][] grid, int row, int col, bool flashing)
+{
+    // Don't do anything if we're out of bounds.
+    if (row < 0 || col < 0) return;
+    if (row >= grid.Length || col >= grid[row].Length) return;
+    //    if (flashing && grid[row][col] == 0) return; // any octopus who already flashed can't flash again this round.
+    if (!flashing) grid[row][col]++;
+    if (grid[row][col] > 9)
+    {
+        flashes++;
+        grid[row][col] = 0; // We flashed. Zero.
+        for (var r2 = row - 1; r2 < row + 2; r2++)
+        {
+            for (var c2 = col - 1; c2 < col + 2; c2++)
+            {
+                if (r2 == row && c2 == col) continue; // don't flash yourself
+                Flash(grid, r2, c2, true);
+            }
+        }
+    }
+}
+
+
+public class Octopus
+{
+    private readonly int row;
+    private readonly int col;
+    private readonly int energy;
+
+    public Octopus(int row, int col, int energy)
+    {
+        this.row = row;
+        this.col = col;
+        this.energy = energy;
+    }
+
+
+
+    public List<Octopus> Neighbours { get; set; } = new List<Octopus>();
+}
--- day17/Program.cs
+++ day17/Program.cs
@@ -1,24 +1,47 @@
 ﻿using System.Text.RegularExpressions;
 using System.Linq;
 // See https://aka.ms/new-console-template for more information
-var match = Regex.Match(File.ReadAllText("example.txt"), @"target area: x=([-0-9]+)\.\.([-0-9]+), y=([-\d]+)\.\.([-\d]+)");
-var extents = match.Groups.Values.Skip(1).Select(m => Int32.Parse(m.Value));
+var match = Regex.Match(File.ReadAllText("input.txt"), @"target area: x=([-0-9]+)\.\.([-0-9]+), y=([-\d]+)\.\.([-\d]+)");
+var extents = match.Groups.Values.Skip(1).Select(m => Int32.Parse(m.Value)).ToArray();
 
-var dyMax = 0;
-for (var dx = 0; dx < 100; dx++) {
-    for (var dy = 0; dy < 100; dy++) {
-        var x = 0, y = 0;
-        for (var step = 0; step < 100; step++) {
-            if (hit(x, y, extents))
+List<int> solvePart1(int[] extents) {
+    List<int> yMaxes = new List<int>();
+    var dyMax = 0;
+    for (var dxInitial = -1000; dxInitial < 1000; dxInitial++) {
+        Console.Write(".");
+        for (var dyInitial = -1000; dyInitial < 1000; dyInitial++) {
+            // Console.WriteLine($"Testing ({dxInitial},{dyInitial}):");
+            int x = 0, y = 0, dx = dxInitial, dy = dyInitial;
+            int yMax = 0;
+            for (var step = 0; step < 1000; step++) {
+                yMax = Math.Max(y, yMax);
+                if (hit(x, y, extents)) {
+                    Console.WriteLine($"Scored a hit with ({dxInitial},{dyInitial}) after {step} steps! (yMax: {yMax})");
+                    yMaxes.Add(yMax);
+                    dyMax = Math.Max(dy, dyMax);
+                    break;
+                }
                 x += dx;
-            y += dy;
-
-
+                y += dy;
+                if (x > extents[1] || y < extents[2]) break;
+                dy -= 1;
+                dx += dx switch {
+                    > 0 => -1,
+                    < 0 => +1,
+                    _ => 0
+                };
+            }
         }
     }
+    return yMaxes;
 }
 
+var maxima = solvePart1(extents);
+var highest = maxima.Max();
+
+Console.WriteLine($"Part 1: {highest}");
+Console.WriteLine($"Part 2: {maxima.Count}");
 
 bool hit(int x, int y, int[] extents) {
-    return (extents[0] <= x && x <= extents[1] && extents[2] <= y && y >= extents[3]);
+    return (extents[0] <= x && x <= extents[1] && extents[2] <= y && y <= extents[3]);
 }
